a79ddb97f7a724f701f8011ea978842097690503
Derek Lamego <lamego.derek@outlook.com>
2021-05-18 21:12:04 -0300

    Modal nos cards
    Modal adicionado nos cards dos estabelecimentos

--- src/views/MapScreen.js
+++ src/views/MapScreen.js
@@ -1,20 +1,66 @@
-import React , {useState,useEffect} from 'react';
+import React , {useState,useEffect,useRef} from 'react';
+import { Text, View, TouchableOpacity, SafeAreaView,ScrollView,StyleSheet, Dimensions,Image,Animated} from 'react-native';
 import Estilo from '../components/estilo';
 import GlobalStyles from '../components/GlobalStyles';
-import { Text, View, TouchableOpacity, SafeAreaView,ScrollView,StyleSheet, Dimensions,Image} from 'react-native';
 import MapView from 'react-native-maps';
 import * as Location from 'expo-location';
-import { Value } from 'react-native-reanimated';
-import { render } from 'react-dom';
+import {Modalize} from 'react-native-modalize'
+// import { Value } from 'react-native-reanimated';
+// import { render } from 'react-dom';
 
 const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 const CARD_WIDTH = windowWidth;
 
 export default ({navigation}) => {
     const [location, setLocation] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
     const [origin, setOrigin] = useState(null);
+    const modalizeRef = useRef(null);
+//------------------------------------------------------------------------------------//
+    // const [alignment] = useState(new Animated.Value(0));
+
+    // const bringUpCard = () => {
+    //     Animated.timing(alignment, {
+    //         toValue:1,
+    //         duration:500
+    //     }).start();
+    // };
+
+    // const hideCard = () => {
+    //     Animated.timing(alignment, {
+    //         toValue:0,
+    //         duration:500
+    //     }).start();
+    // };
+
+    // const cardInterpolate = alignment.interpolate({
+    //     inputRange:[0,1],
+    //     outputRange: [windowHeight/1.5,0]
+    // });
+
+    // const cardStyle={
+    //     transform:[
+    //         {
+    //             translateY: cardInterpolate
+    //         }
+    //     ]
+       
+    // }
+
+    // const gestureHandler = (e) => {
+    //     if(e.nativeEvent.contentOffset.y>0){
+    //     bringUpCard();
+    //     console.warn('11111')     
+    //     }else if(e.nativeEvent.contentOffset.y<0){
+    //         hideCard();
+    //         console.warn('22222')   
+    //     } 
+    // }
+//----------------------------------------------------------------------------------------------
+
     
+
 
     //Lugares adicionados em um array, possivelmente irá pra um banco de dados
     state ={ 
@@ -22,7 +68,10 @@ export default ({navigation}) => {
             {
             id:1,
             title: 'Health Valley Barra',
-            description: 'Loja e restaurante de produtos naturais',
+            description: 'Loja e restaurante de produtos naturais.',
+            address:'R. Afonso Celso, 287 - Barra, Salvador - BA, 40150-480',
+            serviceTime:'Segunda à Sexta - 8:30 às 17:00 | Sábado - 8:30 às 15:00',
+            tel:'(71) 99688-6465',
             latitude:-13.008381269148742, 
             longitude:-38.52953320267205,
             image: require('../img/healthbarra.png')
@@ -106,6 +155,10 @@ export default ({navigation}) => {
         this.state.places[0].mark.showCallout();
     };
 
+    function onOpen(){
+        modalizeRef.current?.open();
+    }
+
     const onMarkerPress = (mapEventData) => {
         const markerID = mapEventData._targetInst.return.key-1;
 
@@ -133,47 +186,47 @@ export default ({navigation}) => {
         })();
     }, []);
 
-    
-        const { latitude, longitude,mark } = this.state.places[0];
-        const _scrollView = React.useRef(null);
-        return(
-            //conteudo da pagina
-            <SafeAreaView style={[Estilo.AppPrincipal, GlobalStyles.AndroidSafeArea]}>
-                {/*Header da pagina */}
-                <View style={[Estilo.HeadColor, {flexDirection:'row'}]}>
-                    <TouchableOpacity onPress={() => navigation.goBack()} >
-                        <Text style={[Estilo.txtBack,{textDecorationLine: 'underline'}] }>Voltar</Text>
-                    </TouchableOpacity>
+    const { latitude, longitude,mark } = this.state.places[0];
+    const _scrollView = React.useRef(null);
 
-                    <View style={[Estilo.HeadAlign]}>
-                        <Text style={Estilo.TextHead}>Busque aqui</Text>
-                    </View>
+    return(
+        //conteudo da pagina
+        <SafeAreaView style={[Estilo.AppPrincipal, GlobalStyles.AndroidSafeArea]}>
+            {/*Header da pagina */}
+            <View style={[Estilo.HeadColor, {flexDirection:'row'}]}>
+                <TouchableOpacity onPress={() => navigation.goBack()} >
+                    <Text style={[Estilo.txtBack,{textDecorationLine: 'underline'}] }>Voltar</Text>
+                </TouchableOpacity>
+
+                <View style={[Estilo.HeadAlign]}>
+                    <Text style={Estilo.TextHead}>Busque aqui</Text>
                 </View>
+            </View>
 
-                {/* Mapa */}
-                <View style={EstiloLocal.container}>
-                    <MapView style={Estilo.map}
-                            ref={map => this.mapView = map}
-                            initialRegion={origin}
-                            showsUserLocation={true}
-                            showsPointsOfInterest={false}
-                            showsBuildings={false}
-                            customMapStyle={[{
-                                            "featureType": "poi.business",
-                                            "stylers": [{
-                                                "visibility": "off"
-                                            }
-                                            ]},
+            {/* Mapa */}
+            <View style={EstiloLocal.container}>
+                <MapView style={Estilo.map}
+                        ref={map => this.mapView = map}
+                        initialRegion={origin}
+                        showsUserLocation={true}
+                        showsPointsOfInterest={false}
+                        showsBuildings={false}
+                        customMapStyle={[{
+                                        "featureType": "poi.business",
+                                        "stylers": [{
+                                            "visibility": "off"
+                                        }
+                                        ]},
 
-                                            {
-                                            "featureType": "poi.park",
-                                            "elementType": "labels.text",
-                                            "stylers": [{
-                                                "visibility": "off"
-                                            }]
-                                            }
-                                        ]}
-                    >
+                                        {
+                                        "featureType": "poi.park",
+                                        "elementType": "labels.text",
+                                        "stylers": [{
+                                            "visibility": "off"
+                                        }]
+                                        }
+                                    ]}
+                >
                     {this.state.places.map(places =>(
                         <MapView.Marker
                             ref={mark => places.mark = mark}
@@ -188,13 +241,15 @@ export default ({navigation}) => {
                             image={require('../img/leaf.png')}
                         />
                     ))}
-                    
-                </MapView>
                 
+                </MapView>
+        
+                {/* onScroll={(e)=>gestureHandler(e)} */}
+                {/* onScroll={(e)=>gestureHandler(e)} */}
                 {/* Scroll dos lugares no mapa */}
-             <ScrollView 
+                <ScrollView 
+                    style={[Estilo.placesContainer]}
                     ref={_scrollView}
-                    style={Estilo.placesContainer}
                     horizontal
                     pagingEnabled
                     //Função para mudar a localização de acordo com os cards
@@ -205,11 +260,12 @@ export default ({navigation}) => {
                         const {latitude,longitude,mark} = this.state.places[parseInt(place+1)];
                         
                         this.mapView.animateCamera({
-                              center: {
-                                latitude,
-                                longitude
-                              }
+                                center: {
+                                    latitude,
+                                    longitude
+                                }, 
                             },
+                        
                             1000
                         );
 
@@ -219,29 +275,57 @@ export default ({navigation}) => {
                     }}
                 >
                     {this.state.places.map(places =>(
-                    
-                        <View key={places.id} style={Estilo.places}>
+
+                        <View key={places.id} style={[Estilo.places]}>
                             <View>
+                                <TouchableOpacity style={EstiloLocal.grabber} onPress={onOpen}></TouchableOpacity>
                                 <Image style={EstiloLocal.image} source={places.image}/>
                             </View>
-                            <View>
+                            <View >
                                 <Text style={Estilo.txtM}>{places.title}</Text>
-                                <Text style={EstiloLocal.txtPlaces}>{places.description}</Text>
-                            </View>
+
+                                <Text style={EstiloLocal.txtPlaces}>
+                                    {places.description} {'\n'}
+                                </Text>
+                            
+                                {/* <Text style={EstiloLocal.txtInfo} >
+                                    <Text style={EstiloLocal.titleDescription}>Endereço: </Text>{places.address} {'\n'}
+                                    <Text style={EstiloLocal.titleDescription}>Telefone: </Text>{places.tel}
+                                </Text> */}
                                 
+                            </View> 
+
                         </View>
-                        
 
                     ))}
                         
                 </ScrollView>
-                </View>
 
-                {/*Footer da pagina */}
-                <View style={Estilo.BottomColor}></View>
-            </SafeAreaView>
-        )
-    
+                <Modalize
+                    ref={modalizeRef}
+                    snapPoint={500}
+                    modalHeight={500}
+                >
+                    <View 
+                        style={{ 
+                            flex:1,
+                            height:500,
+                            flexDirection: 'row',
+                            position:'absolute'
+                        }}>
+                            <Text style={Estilo.txtM}>EXEMPLO</Text>
+
+                            <Text style={EstiloLocal.txtPlaces}>
+                                DESCRIÇÃO EXEMPLO
+                            </Text>
+                    </View> 
+                </Modalize>
+                           
+            </View>
+            {/*Footer da pagina */}
+            <View style={Estilo.BottomColor}></View>
+        </SafeAreaView>
+    )
 };
 
 const EstiloLocal = StyleSheet.create({
@@ -259,9 +343,28 @@ const EstiloLocal = StyleSheet.create({
     txtPlaces:{
         fontSize: 16,
         color: '#544F1F',
-        marginBottom:20,
+        marginBottom:10,
         paddingLeft:20,
         paddingRight:20,
         textAlign: 'center',
+    },
+    titleDescription:{
+        fontWeight:'bold',
+    
+    },
+    txtInfo:{
+        marginLeft: 10,
+        color: '#544F1F',
+        fontSize:16
+    },
+    grabber:{
+        marginTop:10,
+        position:'absolute',
+        zIndex:1,
+        alignSelf: 'center',
+        width:85,
+        borderRadius:5,
+        borderTopWidth:20,
+        borderTopColor:'white'
     }
 })
