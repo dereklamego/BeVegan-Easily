48feab490c69c04905b39eeef943a0edb8501285
Derek Lamego <lamego.derek@outlook.com>
2021-05-31 21:49:51 -0300

    Reparação de bug
    Bug na scrollview

--- src/views/WhatIsScreen.js
+++ src/views/WhatIsScreen.js
@@ -16,34 +16,34 @@ export default ({navigation}) => {
                         <Text style={[Estilo.TextHead,{textAlign:'center'}]}> O que é veganismo</Text>
                 </View>
             </View>
-            <ScrollView showsVerticalScrollIndicator={false} style={{flex:'row'}}>
-             <View style={[EstiloLocal.imgContainer]}>
-                 <Image id = 'minha img' source={require('../img/Whatisss.jpg')} style={[EstiloLocal.img]}/>
-             </View>
-            <View style={[EstiloLocal.textContainer]}>
-               
-                <Text style={[EstiloLocal.txtTitle]}>Veganismo</Text>
-                <Text style={[EstiloLocal.txt]}>
-                    É uma filosofia e estilo de vida que busca excluir, na medida do possível e praticável, todas as
-                    formas de exploração e crueldade contra animais na alimentação, vestuário e qualquer outra
-                    finalidade e por extensão, que promova o desenvolvimento e uso de alternativas livres de origem
-                    animal para benefício de humanos, animais e meio ambiente.
-        
-                </Text>
-                <Text style={[EstiloLocal.txt]}>
-                Assim, além de não consumir nenhum tipo de carne, os veganos também retiram da sua rotina todos os
-                alimentos e produtos de origem animal ou que tenham causado algum tipo de sofrimento aos animais.
-                </Text>
+            <ScrollView showsVerticalScrollIndicator={false} >
+                <View style={[EstiloLocal.imgContainer]}>
+                    <Image source={require('../img/Whatisss.jpg')} style={[EstiloLocal.img]}/>
+                </View>
+                <View style={[EstiloLocal.textContainer]}>
                 
-                <Text style={[EstiloLocal.txtTitle]}> o Video vai ser aqui </Text>
-                <Text style={[EstiloLocal.txt]}>
-                Por isso, o vegano também exclui da sua dieta os laticínios, ovos e mel. Além de não utilizar
-                nenhum tipo de produto de origem animal, seja de higiene, cosméticos, vestuário, medicinais, entre outros.
-                </Text>
+                    <Text style={[EstiloLocal.txtTitle]}>Veganismo</Text>
+                    <Text style={[EstiloLocal.txt]}>
+                        É uma filosofia e estilo de vida que busca excluir, na medida do possível e praticável, todas as
+                        formas de exploração e crueldade contra animais na alimentação, vestuário e qualquer outra
+                        finalidade e por extensão, que promova o desenvolvimento e uso de alternativas livres de origem
+                        animal para benefício de humanos, animais e meio ambiente.
+            
+                    </Text>
+                    <Text style={[EstiloLocal.txt]}>
+                    Assim, além de não consumir nenhum tipo de carne, os veganos também retiram da sua rotina todos os
+                    alimentos e produtos de origem animal ou que tenham causado algum tipo de sofrimento aos animais.
+                    </Text>
+                    
+                    <Text style={[EstiloLocal.txtTitle]}> o Video vai ser aqui </Text>
+                    <Text style={[EstiloLocal.txt]}>
+                    Por isso, o vegano também exclui da sua dieta os laticínios, ovos e mel. Além de não utilizar
+                    nenhum tipo de produto de origem animal, seja de higiene, cosméticos, vestuário, medicinais, entre outros.
+                    </Text>
 
+                    
                 
-              
-            </View>
+                </View>
             </ScrollView>
             {/*Footer da pagina */}
             <View style={Estilo.BottomColor}></View>
@@ -89,10 +89,7 @@ const EstiloLocal= StyleSheet.create({
         },
 
         textContainer:{
-                backgroundColor: 'white',
-
-
-
+             backgroundColor: 'white',
         }
 
 
